83f358138c3a5c035c88f09f748d3b76b0453d9a
Michael Reefe <57511178+Michael-Reefe@users.noreply.github.com>
2021-03-04 21:09:38 -0500

    added color-color plots for ex. 5

--- sdss_exercise_3-5/sdss_ex3-5.py
+++ sdss_exercise_3-5/sdss_ex3-5.py
@@ -13,18 +13,15 @@ data['ri'] = data['modelmag_r'] - data['modelmag_i']
 data['iz'] = data['modelmag_i'] - data['modelmag_z']
 
 # Example plot for exercise 3
-plt.style.use('ggplot')
-fig, ax = plt.subplots()
-data.plot(x='ug', y='gr', kind='scatter', xlabel='$u - g$ [mag]', ylabel='$g - r$ [mag]', title='Quasar Color-Color Diagram')
-plt.savefig('quasar-color-color.png', dpi=300)
-plt.close()
+# ???????????????????????????
 
 # Plots for exercise 4
 # Scatterplots
+plt.style.use('ggplot')
 x_data = np.array(['ug', 'gr', 'ri', 'iz'], dtype=str).reshape((2, 2))
 fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(8, 8), sharey=True)
 for i, j in itertools.product(range(2), range(2)):
-    data.plot(x=x_data[i, j], y='z', kind='scatter', ax=axes[i, j], xlabel='$%s$ [mag]' % (x_data[i, j][0] + ' - ' + x_data[i, j][1]),
+    data.plot(x=x_data[i, j], y='z', kind='scatter', ax=axes[i, j], logy=True, xlabel='$%s$ [mag]' % (x_data[i, j][0] + ' - ' + x_data[i, j][1]),
               ylabel='Redshift')
 fig.suptitle('Quasar Color-Redshift Diagrams')
 plt.savefig('quasar-color-redshift.png', dpi=300)
@@ -49,7 +46,7 @@ def make_density_plot(norm, label: str = None, **kwargs):
 
         deltaX = (np.nanmax(x) - np.nanmin(x)) / 10
         deltaY = (np.nanmax(y) - np.nanmin(y)) / 10
-        ymin = 0
+        ymin = np.nanmin(y)
         ymax = np.nanmax(y) + deltaY
         xmin = np.nanmin(x) - deltaX
         xmax = np.nanmax(x) + deltaX
@@ -58,14 +55,15 @@ def make_density_plot(norm, label: str = None, **kwargs):
             xlim = kwargs['xlim']
             xmin = xlim[0]
             xmax = xlim[1]
-            axes[i, j].set_xlim(xmin, xmax)
             axes[i, j].set_xticks(np.arange(xmin, xmax + 0.5, 0.5))
+        axes[i, j].set_xlim(xmin, xmax)
         if 'ylim' in kwargs.keys():
             assert len(kwargs['ylim']) == 2, "x should be of the form (xmin, xmax)"
             ylim = kwargs['ylim']
             ymin = ylim[0]
             ymax = ylim[1]
-            axes[i, j].set_ylim(ymin, ymax)
+        axes[i, j].set_ylim(ymin, ymax)
+        axes[i, j].set_yscale('log')
 
         # Create meshgrid
         xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
@@ -95,5 +93,27 @@ def make_density_plot(norm, label: str = None, **kwargs):
     plt.close()
 
 
-make_density_plot(norm=colors.Normalize(vmin=0, vmax=1.6), label='Linear Scaling', xlim=(-0.5, 1.0), ylim=(0, 3))
+make_density_plot(norm=colors.Normalize(vmin=0, vmax=1.6), label='Linear Scaling', xlim=(-0.5, 1.0), ylim=(1e-01, 3))
 make_density_plot(norm=colors.LogNorm(), label='Logarithmic Scaling')
+
+
+# Color-color diagrams for Exercise 5
+def plot_color_color(xcolor: str, ycolor: str, axis):
+    assert xcolor in ('ug', 'gr', 'ri', 'iz') and ycolor in ('ug', 'gr', 'ri', 'iz')
+    scatterplot = axis.scatter(data[xcolor], data[ycolor], c=data['z'], cmap='coolwarm', norm=colors.Normalize(vmin=np.nanmin(data['z']), vmax=np.nanmax(data['z'])))
+    axis.set_xlabel('$%s$ [mag]' % (xcolor[0] + ' - ' + xcolor[1]))
+    axis.set_ylabel('$%s$ [mag]' % (ycolor[0] + ' - ' + ycolor[1]))
+    return scatterplot
+
+
+plt.style.use('ggplot')
+fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(18, 6))
+sp1 = plot_color_color('ug', 'gr', ax[0])
+sp2 = plot_color_color('gr', 'ri', ax[1])
+sp3 = plot_color_color('ri', 'iz', ax[2])
+pos1 = ax[2].get_position()
+cbar_ax = fig.add_axes([0.92, pos1.y0, 0.012, pos1.height])
+fig.colorbar(sp3, cbar_ax, label='Redshift ($z$)')
+fig.suptitle('Quasar Color-Color Diagrams')
+plt.savefig('quasar-color-color.png', dpi=300, bbox_inches='tight')
+plt.close()
